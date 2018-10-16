c72be5762524eda7108842c47475e974a288482f
Kevin Gruber <kevin-gruber1@gmx.at>
2018-10-16 16:50:22 +0200

    added Assignmentpy03 to work on it til 21.10.2018

---
+++ Python/Übungen/Assignment3/Assignmentpy03.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Requirement for Python Upload 03 about regex and generators:
+#
+# a) Comments required: Add your Name
+#    remove this comments or other unused lines of code
+
+# b) For regular expressions:
+#    b1) extract all emails from the given html source
+#    b2) return all teachers (firstname, lastname) from given html source
+#
+#    Just for your information: this is (a slightly modified) extracted html snippet from https://archiv.fh-joanneum.at/aw/home/Studienangebot-Uebersicht/department-angewandte-informatik/swd/Menschen/Team/~mcq/lehrpersonal/?lan=de )
+
+# c) For generators
+#    c1) generate filenames "2018_img_001.jpg", "2018_img_002.png"..."2018_img_999.svg"
+
+# NOTE: "evaluate" works just within the FH Network! Maybe you want to use VPN.
+
+def extractEmailsFromHTML(src):
+    result = []
+    #### START of my CODE ####
+
+    # find with regex emails (e.g. norah*smith#edu*fh-joanneum*at )
+    # repair emails (by replacing characters *, #)
+    # and add those to the result (list)
+
+    #### END of my CODE ####
+    return result
+
+
+def extractNamesFromHTML(src):
+    result = []
+    #### START of my CODE ####
+
+    # find with regex (firstname/lastname)-tuples
+    # and add to the result (list):
+    # [ ("Norah","Smith"), (...,...), (...,...) ]
+
+    #### END of my CODE ####
+    return result
+
+
+def fileNameGenerator(year=2021, suffix="jpg", count=99):
+    #### START of my CODE ####
+
+    # yield x-times something like 2018_img_005.svg ...
+    yield "TODO"
+    #### END of my CODE ####
+
+
+# Just for testing:
+
+# (the moodle-"evaluate" will use the exact same test data!!)
+htmlSource = open("team_itm.html", encoding='utf-8').read()
+# print(htmlSource) # ...<h2><br /><strong>Norah Smith</strong><br/> ...onclick="javascript:anschreiben('norah*smith#edu*fh-joanneum*at')" onmouseover=...
+
+for firstname, lastname in extractNamesFromHTML(htmlSource):
+    print(lastname)  # Smith
+
+for email in extractEmailsFromHTML(htmlSource):
+    print(email)  # norah.smith@edu.fh-joanneum.at
+
+# (the moodle-"evaluate" might run with different test data!!)
+for bc in fileNameGenerator(2016, "png", 4):
+    print(bc)  # prints: "2016_img_001.png",...,"2016_img_004.png"
+
+for bc in fileNameGenerator(2019, "svg", 9):
+    print(bc)  # prints: "2019_img_001.svg",...,"2019_img_009.svg"
+

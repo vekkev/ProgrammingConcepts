7dd05425f5879b411738a4d297865c4398cb536e
Kevin Gruber <kevin-gruber1@gmx.at>
2018-12-08 13:37:10 +0100

    added python_week folder and first file

---
+++ Python_Week/1of7_sum-up-the-numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import sys
+# 2018 by the famous python coder: <enter your name here> 
+res = "NOT IMPLEMENTED YET" # You might consider slices with [:], argv, map, sum, int, ... (no error handling required)
+print("Python Week 1/7: The sum of command line arguments {} is '{}'.".format(sys.argv[1:],res))

96df8279abbe3e3ce7e4571a6bbe2c1a2d6ab7f0
Kevin Gruber <kevin-gruber1@gmx.at>
2018-12-09 20:04:04 +0100

    finished pythonWeek1 challenge

--- Python_Week/1of7_sum-up-the-numbers.py
+++ Python_Week/1of7_sum-up-the-numbers.py
@@ -5,3 +5,4 @@ import sys
 res = int(sys.argv[1]) + int(sys.argv[2])  # You might consider slices with [:], argv, map, sum, int, ... (no error handling required)
 
 print("Python Week 1/7: The sum of command line arguments {} is '{}'.".format(sys.argv[1:], res))
+

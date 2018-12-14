336b869342878541c3115ac00b2217278827af9e
kevingruber <kevin-gruber1@gmx.at>
2018-12-14 18:27:47 +0100

    updated file with fibonacci to work on

--- C/Übungen/exercise_1_Gruber/main.c
+++ C/Übungen/exercise_1_Gruber/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+long fibonacci(long n) {
+    if (n < 2) {
+        return n;
+    }
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2)
+    {
+        printf("usage fib <number>\n");
+        return 1;
+    }
+
+    int value = atoi(argv[1]);
+    int result = fibonacci(value);
+    printf("fib(%d) = %d\n",value,result);
+
     return 0;
 }
+

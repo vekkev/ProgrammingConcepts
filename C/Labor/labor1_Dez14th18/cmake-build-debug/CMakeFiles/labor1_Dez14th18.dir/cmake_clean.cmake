fb56f4dd7b2aed93060b789ae3c6590ded3ce3d6
kevingruber <kevin-gruber1@gmx.at>
2018-12-14 18:10:50 +0100

    added C folder

---
+++ C/Labor/labor1_Dez14th18/cmake-build-debug/CMakeFiles/labor1_Dez14th18.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labor1_Dez14th18.dir/main.c.o"
+  "labor1_Dez14th18.pdb"
+  "labor1_Dez14th18"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/labor1_Dez14th18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/Labor/labor1_Dez14th18/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]){
+    int x;
+    printf("%d\n", x);
+    return 0;
+}

cf136314824fb9c71ce943974054660e2446eb36
kevingruber <kevin-gruber1@gmx.at>
2019-01-22 22:56:35 +0100

    created exercise_2_Gruber to work on

---
+++ C/Übungen/Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment1.dir/main.c.o"
+  "Assignment1.pdb"
+  "Assignment1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
